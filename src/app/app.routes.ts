7eafee3380a4c7045bbe8aeef6b032b36702a8fb
Nemesis502 <klawes-bastian@t-online.de>
2025-05-29 13:31:29 +0200

    added intro scree n, bevor log in page. added favicon, set standard Color in styles.scss.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { MainContentComponent } from './main-content/main-content.component';
+import { LogingPageComponent } from './main-content/loging-page/loging-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: LogingPageComponent },
+];
---
+++ src/app/main-content/loging-page/loging-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loging-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './loging-page.component.html',
+  styleUrl: './loging-page.component.scss'
+})
+export class LogingPageComponent {
+
+}
