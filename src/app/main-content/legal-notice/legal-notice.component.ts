adaf88e3ec6c3ae396c1e1b95840579e9297c81a
ErikMic <erikmic@hotmail.de>
2025-06-01 12:50:10 +0200

    add logo and legal-notice

---
+++ src/app/main-content/legal-notice/legal-notice.component.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { LogoComponent } from '../shared/logo/logo.component';
+
+@Component({
+  selector: 'app-legal-notice',
+  standalone: true,
+  imports: [
+    CommonModule,
+    LogoComponent
+  ],
+  templateUrl: './legal-notice.component.html',
+  styleUrl: './legal-notice.component.scss'
+})
+export class LegalNoticeComponent {
+
+}
