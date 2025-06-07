c3f40e6c1f39af9658bcb44750487fcfc34c0589
ErikMic <erikmic@hotmail.de>
2025-06-07 14:52:28 +0200

    add direct message

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { LogingPageComponent } from './main-content/loging-page/loging-page.comp
 import { SingInPageComponent } from './main-content/loging-page/sing-in-page/sing-in-page.component';
 import { ChooseAvatarPageComponent } from './main-content/loging-page/sing-in-page/choose-avatar-page/choose-avatar-page.component';
 import { ChatTemplateComponent } from './main-content/chat-template/chat-template.component';
+import { MainMenuComponent } from './main-content/main-menu/main-menu.component';
 
 export const routes: Routes = [
-    { path: '', component: LogingPageComponent },
+    // { path: '', component: LogingPageComponent },
+    { path: '', component: MainMenuComponent },
     { path: 'singIn', component: SingInPageComponent },
     { path: 'singIn/chooseAvatar', component: ChooseAvatarPageComponent },
     { path: 'chat', component: ChatTemplateComponent }
