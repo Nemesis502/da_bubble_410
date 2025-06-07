b1fcfc89095260b4621c5bc119aeb5cb83ccb5ee
ErikMic <erikmic@hotmail.de>
2025-06-07 15:54:31 +0200

    add legal notics route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { SingInPageComponent } from './main-content/loging-page/sing-in-page/sin
 import { ChooseAvatarPageComponent } from './main-content/loging-page/sing-in-page/choose-avatar-page/choose-avatar-page.component';
 import { ChatTemplateComponent } from './main-content/chat-template/chat-template.component';
 import { MainMenuComponent } from './main-content/main-menu/main-menu.component';
+import { LegalNoticeComponent } from './main-content/legal-notice/legal-notice.component';
 
 export const routes: Routes = [
     { path: '', component: LogingPageComponent },
     { path: 'singIn', component: SingInPageComponent },
     { path: 'singIn/chooseAvatar', component: ChooseAvatarPageComponent },
     { path: 'chat', component: ChatTemplateComponent },
-     { path: 'main', component: MainMenuComponent },
+    { path: 'main', component: MainMenuComponent },
+    { path: 'legal-notice', component: LegalNoticeComponent },
 ];
