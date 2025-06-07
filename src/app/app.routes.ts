e591d6860583f53f074e6a01ebafc2d805db548f
Niklas Lampe <125186474+NiklasLa46@users.noreply.github.com>
2025-06-07 15:40:43 +0200

    new route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { LogingPageComponent } from './main-content/loging-page/loging-page.comp
 import { SingInPageComponent } from './main-content/loging-page/sing-in-page/sing-in-page.component';
 import { ChooseAvatarPageComponent } from './main-content/loging-page/sing-in-page/choose-avatar-page/choose-avatar-page.component';
 import { ChatTemplateComponent } from './main-content/chat-template/chat-template.component';
+import { MainMenuComponent } from './main-content/main-menu/main-menu.component';
 
 export const routes: Routes = [
     { path: '', component: LogingPageComponent },
     { path: 'singIn', component: SingInPageComponent },
     { path: 'singIn/chooseAvatar', component: ChooseAvatarPageComponent },
-    { path: 'chat', component: ChatTemplateComponent }
+    { path: 'chat', component: ChatTemplateComponent },
+     { path: 'main', component: MainMenuComponent },
 ];
