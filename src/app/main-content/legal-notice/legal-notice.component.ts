d0d6ce31f9e86b6305defb9ffe91a68cb908bb31
Nemesis502 <klawes-bastian@t-online.de>
2025-07-30 16:42:29 +0200

    imprint now responsive

--- src/app/main-content/legal-notice/legal-notice.component.ts
+++ src/app/main-content/legal-notice/legal-notice.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Location } from '@angular/common';
 import { LogoComponent } from '../../shared/logo/logo.component';
 import { Router } from '@angular/router';
 
@@ -8,15 +9,16 @@ import { Router } from '@angular/router';
   standalone: true,
   imports: [
     CommonModule,
-    LogoComponent
+    LogoComponent,
   ],
   templateUrl: './legal-notice.component.html',
   styleUrl: './legal-notice.component.scss'
 })
 export class LegalNoticeComponent {
-  constructor(private router: Router) { }
+  constructor(private router: Router, private location: Location) { }
 
   goBack() {
-    this.router.navigate(['/']);
+    // this.router.navigate(['/']);
+    this.location.back();
   }
 }
