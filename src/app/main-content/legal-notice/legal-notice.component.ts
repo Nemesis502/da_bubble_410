7eac9bcb851f2fed127c005d47209ea0a709a436
ErikMic <erikmic@hotmail.de>
2025-09-10 18:22:47 +0200

    fixed back button from privacy policy and legal notice

--- src/app/main-content/legal-notice/legal-notice.component.ts
+++ src/app/main-content/legal-notice/legal-notice.component.ts
@@ -2,6 +2,8 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Location } from '@angular/common';
 import { LogoComponent } from '../../shared/logo/logo.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-legal-notice',
@@ -9,6 +11,8 @@ import { LogoComponent } from '../../shared/logo/logo.component';
   imports: [
     CommonModule,
     LogoComponent,
+    MatButtonModule,
+    MatIconModule
   ],
   templateUrl: './legal-notice.component.html',
   styleUrl: './legal-notice.component.scss'
--- src/app/main-content/privacy-policy/privacy-policy.component.ts
+++ src/app/main-content/privacy-policy/privacy-policy.component.ts
@@ -2,20 +2,23 @@ import { CommonModule, ViewportScroller } from '@angular/common';
 import { Component } from '@angular/core';
 import { Location } from '@angular/common';
 import { LogoComponent } from '../../shared/logo/logo.component';
-import { Router } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-privacy-policy',
   standalone: true,
   imports: [
     CommonModule,
-    LogoComponent
+    LogoComponent,
+    MatButtonModule,
+    MatIconModule
   ],
   templateUrl: './privacy-policy.component.html',
   styleUrl: './privacy-policy.component.scss'
 })
 export class PrivacyPolicyComponent {
-  constructor(private router: Router, private viewportScroller: ViewportScroller, private location: Location) { }
+  constructor(private viewportScroller: ViewportScroller, private location: Location) { }
 
   goTo(id: string) {
     this.viewportScroller.scrollToAnchor(id);
